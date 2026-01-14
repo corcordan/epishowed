0d48382b18460034af0d3c23cb125476ad7e5916
Daniel Corcoran <corcordan@gmail.com>
2026-01-13 23:30:31 -0500

    Fixed server-side empty returns

--- app/show/[id]/page.tsx
+++ app/show/[id]/page.tsx
@@ -1,26 +1,36 @@
 import ShowPage from '@/components/show/Show'
 import { fetchPeople, fetchSeasons, fetchShow } from '@/utils/fetchUtils'
 import { getYear } from 'date-fns'
+import { notFound } from 'next/navigation'
 
 export default async function Shows({ params }: { params: Promise<{ id: number }> }) {
 
     const { id } = await params
     const showInfo = await fetchShow(id)
 
-    if (!showInfo) return
+    if (!showInfo) {
+        console.error("fetchShow returned undefined for ID", id)
+        notFound()
+    }
 
     const start = getYear(showInfo.first_air_date).toString()
     const end = showInfo.status === "Ended" ? getYear(showInfo.last_air_date).toString() : "now"
 
     const seasonPromise = await fetchSeasons(showInfo, id)
 
-    if (!seasonPromise) return
+    if (!seasonPromise) {
+        console.error("fetchSeasons returned undefined for ID", id)
+        notFound()
+    }
 
     const { seasonInfo, boxHeight, boxWidth, heightSVG, widthSVG, marginSize } = seasonPromise
 
     const peopleInfo = await fetchPeople(seasonInfo, id)
 
-    if (!peopleInfo) return
+    if (!peopleInfo) {
+        console.error("fetchPeople returned undefined for ID", id)
+        notFound()
+    }
 
     return (
         <ShowPage 
